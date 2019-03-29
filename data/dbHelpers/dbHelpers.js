368f35570949350b3982fb16c9bd0cc88c03b9b3
Sorin Chis <kish.sorin@yahoo.com>
2019-03-29 11:19:55 +0200

    added dbHelpers for getting all action , add a action to database

--- data/dbHelpers/dbHelpers.js
+++ data/dbHelpers/dbHelpers.js
@@ -4,15 +4,28 @@ const dbConfig = require('../../knexfile');
 
 const db = knex(dbConfig.development);
 
+//getting all the project from database
 function getProjects() {
 	return db('projects');
 }
 
+//adding a project to database
 function addProject(project) {
 	return db('projects').insert(project);
 }
 
+//getting all action from database
+function getActions() {
+	return db('actions');
+}
+//adding a action to database
+function addAction(action) {
+	return db('actions').insert(action);
+}
+
 module.exports = {
 	getProjects,
-	addProject
+	addProject,
+	getActions,
+	addAction
 };
